8a776ece6761f601ee5c16c158023b8d0d0d7c06
mygosia <mygosia@gmail.com>
2017-04-07 20:57:29 +0200

    grunt dziala, strona wyglada

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,8 +30,8 @@ module.exports = function (grunt) {
     });
  
  grunt.loadNpmTasks('grunt-browser-sync');
-   grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+ grunt.loadNpmTasks('grunt-sass');
+ grunt.loadNpmTasks('grunt-contrib-watch');
  
-grunt.registerTask('default', ['browserSync', 'watch']);
+grunt.registerTask('default', ['browserSync', 'watch', 'sass']);
 };
